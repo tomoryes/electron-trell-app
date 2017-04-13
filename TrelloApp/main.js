e1564c9d0c4c898c33a3082b3d8121718967092f
tomorrow anan <tomorrowanan@gmail.com>
2017-04-13 19:27:08 +0900

    [update] コピー＆ペーストを実装

--- TrelloApp/main.js
+++ TrelloApp/main.js
@@ -3,11 +3,14 @@
 // Electronのモジュール
 const electron = require("electron");
 
+
 // アプリケーションをコントロールするモジュール
 const app = electron.app;
 
 // ウィンドウを作成するモジュール
 const BrowserWindow = electron.BrowserWindow;
+
+const menu = electron.Menu;
 
 // メインウィンドウはGCされないようにグローバル宣言
 let mainWindow;
@@ -22,18 +25,86 @@ app.on('window-all-closed', function() {
 
 // Electronの初期化完了後に実行
 app.on('ready', function() {
+
+    initializeWindow();
+    installMenu();
+});
+
+
+function initializeWindow() {
     // メイン画面の表示。ウィンドウの幅、高さを指定できる
     mainWindow = new BrowserWindow({
         width: 900,
         height: 600
     });
     mainWindow.loadURL('file://' + __dirname + '/index.html');
-
     // ウィンドウが閉じられたらアプリも終了
     mainWindow.on('closed', function() {
         mainWindow = null;
     });
+}
 
 
-    
-});
+/**
+ *  コピー＆ペーストを実装する
+ *  see http://hacknote.jp/archives/16729/
+ */
+function installMenu()
+{
+    var template = [{
+        label: "Application",
+        submenu: [{
+                label: "About Application",
+                selector: "orderFrontStandardAboutPanel:"
+            },
+            {
+                type: "separator"
+            },
+            {
+                label: "Quit",
+                accelerator: "Command+Q",
+                click: function() {
+                    app.quit();
+                }
+            }
+        ]
+    }, {
+        label: "Edit",
+        submenu: [{
+                label: "Undo",
+                accelerator: "CmdOrCtrl+Z",
+                selector: "undo:"
+            },
+            {
+                label: "Redo",
+                accelerator: "Shift+CmdOrCtrl+Z",
+                selector: "redo:"
+            },
+            {
+                type: "separator"
+            },
+            {
+                label: "Cut",
+                accelerator: "CmdOrCtrl+X",
+                selector: "cut:"
+            },
+            {
+                label: "Copy",
+                accelerator: "CmdOrCtrl+C",
+                selector: "copy:"
+            },
+            {
+                label: "Paste",
+                accelerator: "CmdOrCtrl+V",
+                selector: "paste:"
+            },
+            {
+                label: "Select All",
+                accelerator: "CmdOrCtrl+A",
+                selector: "selectAll:"
+            }
+        ]
+    }];
+
+    menu.setApplicationMenu(menu.buildFromTemplate(template));
+}
